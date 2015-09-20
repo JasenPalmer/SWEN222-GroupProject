0c8afdcec08e0064cffaf8584425b44af1fa1c72
ItsNotAGoodTime <ItsNotAGoodTime@192.168.2.10>
2015-09-20 14:10:20 +1200

    Submitting text works

--- src/ui/panels/ChatBoxPanel.java
+++ src/ui/panels/ChatBoxPanel.java
@@ -1,6 +1,8 @@
 package ui.panels;
 
 import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
 
@@ -11,7 +13,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 
-public class ChatBoxPanel extends JPanel{
+public class ChatBoxPanel extends JPanel implements KeyListener{
 
 	private Image backgroundImage;
 	private JTextArea textArea = new JTextArea();
@@ -25,18 +27,43 @@ public class ChatBoxPanel extends JPanel{
 
 		//Set text area
 		textArea.setEditable(false);
-		textArea.append("Please work\n\n\n\n\n\n\n\n\n\n\n");
 		 
 		//Setup scroll pane
 		scrollPane = new JScrollPane(textArea);
 		scrollPane.setBounds(0,0,450,170);
 		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER );
-		this.add(scrollPane);
-		
+		this.add(scrollPane);	
 
 		//Setup text field
 		textBox = new JTextField();
 		textBox.setBounds(0, 170, 450, 20);
+		textBox.addKeyListener(this);
 		this.add(textBox);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		if(e.getKeyChar() == '\n'){
+			System.out.println("enter");
+			appendTextBox();
+		}
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void appendTextBox(){
+		textArea.append(textBox.getText());
+		textBox.setText("");
+		textArea.append("\n");
 	}
 }
