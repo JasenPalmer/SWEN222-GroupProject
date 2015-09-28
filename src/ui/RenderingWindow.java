a952b2bc2c23dea4e3132b5727f15a59157c1769
JakeDorne <jakedorne@hotmail.com>
2015-09-29 12:22:35 +1300

    Camera working for 1 direction. 8/8 m8

--- src/ui/RenderingWindow.java
+++ src/ui/RenderingWindow.java
@@ -18,33 +18,37 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class RenderingWindow extends JPanel{
-	
+
 	private Image backgroundImage;
 	private static Image grass;
 	private static Image building;
 	private static Image water;
 	private static Image rock;
-	private Image doorUD;
 	private static Image doorLR;
+	private Image playerImage;
+	private Image doorUD;
 	private Image roofLR;
 	private Image roofUD;
 	private Image roofCornerO;
 	private Image roofCornerI;
-	
+
+	private int cameraX;
+	private int cameraY;
+
 	private Player player;
 	private ApplicationWindow applicationWindow;
-	
+
 	private int TILESIZE = 64;
-	
+
 	Game.Direction direction = Direction.NORTH;
-	
+
 	public RenderingWindow(ApplicationWindow aw){
 		setLayout(null);
 		setBounds(0,0,1050,750);
 		this.applicationWindow = aw;
 		setImages();
 	}
-	
+
 	/**
 	 * Setting images to files in images folder. Will be changed when map parser is working for some tiles. (grass, water, rock will be gone).
 	 */
@@ -60,13 +64,13 @@ public class RenderingWindow extends JPanel{
 			roofUD = ImageIO.read(new File("src/ui/images/buildings/RoofUD.png"));
 			roofCornerO = ImageIO.read(new File("src/ui/images/buildings/RoofCornerO.png"));
 			roofCornerI = ImageIO.read(new File("src/ui/images/buildings/RoofCornerI.png"));
-			
-			
+			playerImage = ImageIO.read(new File("src/ui/images/player/0.png"));
+
 		}catch(IOException e){
 			System.out.println(e.getLocalizedMessage());
 		}
 	}
-	
+
 	/**
 	 * Returns image to match given name
 	 * @param name - type of image wanted
@@ -87,7 +91,7 @@ public class RenderingWindow extends JPanel{
 		}
 		return null;
 	}
-	
+
 	public static Image createImage(String imagename) {
 		Image image = null;
 		try {
@@ -102,70 +106,23 @@ public class RenderingWindow extends JPanel{
 	 * Rotates tile arrays depending on the current viewing direction and then calls the isometric
 	 * renderer on the resulting arrays
 	 */
-	public void paint( Graphics g ) { 
+	public void paint( Graphics g ) {
+
 		super.paint(g);
-		
+
 		Image offscreen = createImage(this.getWidth(), this.getHeight());
-		Graphics offgc = offscreen.getGraphics();		
-		
+		Graphics offgc = offscreen.getGraphics();
+
 		player = applicationWindow.getPlayer();
 		Location l = player.getLocation();
-		
+
 		Tile[][] tiles = l.getTiles();
 		Tile[][] rooms = null;
-		
+
 		if(l instanceof OutsideLocation){
 			OutsideLocation ol = (OutsideLocation) l;
 			rooms = ol.getBuildingTiles();
 		}
-		
-//		// Example location. To be changed later.
-//		Location l = null;
-//		Tile g1 = new FloorTile(null, new Point(0,0), grass);
-//		Tile g2 = new FloorTile(null, new Point(5,5), grass);
-//		Tile w1 = new FloorTile(null, new Point(0,0), water);
-//		Tile r1 = new FloorTile(null, new Point(0,0), rock);
-//		Tile[][] tiles = {
-//				{g1,g1,g1,g1,g1,g1,g1,g1,g1,g1,g1,g1,g1,g1,g1},
-//				{g1,g1,g1,g1,g1,g1,g1,g1,g1,g2,g1,g1,g1,g1,g1},
-//				{g1,g1,g2,g1,g1,g1,g1,g1,g1,g1,g1,g1,g1,w1,g1},
-//				{g1,g1,g1,g1,g1,g1,g1,g1,g1,g1,g1,g1,w1,w1,g1},
-//				{g1,g1,g1,g1,g1,g1,g1,g1,g1,g1,g1,w1,w1,w1,g1},
-//				{g1,g1,r1,g1,g1,r1,r1,r1,g1,g1,g1,w1,w1,w1,g1},
-//				{r1,r1,r1,r1,g1,r1,r1,g1,g1,g1,g1,g1,w1,w1,g1},
-//				{r1,r1,r1,r1,r1,r1,r1,g1,g2,g1,g1,w1,w1,g1,g1},
-//				{w1,w1,r1,r1,g1,g1,g1,g1,g1,g1,g1,w1,g1,g1,g1},
-//				{w1,w1,w1,r1,g1,g1,g1,g1,g1,w1,w1,w1,g1,g1,g1},
-//				{g1,w1,r1,r1,r1,g1,g1,g1,g1,w1,w1,w1,g1,g1,g1},
-//				{g1,w1,w1,w1,w1,w1,g1,g1,g1,g1,g1,w1,w1,g1,g1},
-//				{g1,w1,w1,g1,w1,w1,g1,g1,g1,g1,g1,g1,w1,w1,g1},
-//				{g1,g1,g1,g1,w1,w1,g1,g1,g1,g1,g1,g1,w1,w1,g1},
-//				{g1,g1,g1,g1,w1,w1,g1,g1,g1,g1,g1,g1,w1,g1,g1},
-//				};
-//		
-//		Tile r = new BuildingTile(null, new Point(1,2), building);
-//		Tile d = new EntranceExitTile(null, new Point(1,2), null, false);
-//		Tile[][] rooms = {	{null,null,null,null,null,null,null,null,null,null,null,null,null,null,null},
-//							{null,null,null,null,null,null,null,null,null,null,null,null,null,null,null},
-//							{null,null,null,null,r,r,r,r,r,r,r, r,r,null,null},
-//							{null,null,null,null,d,r,r,r,r,r,r,r,r,null,null},
-//							{null,null,null,null,r,r,null,null,null,null,null,r,r,null,null},
-//							{null,null,null,null,r,r,null,null,null,null,null,r,r,null,null},
-//							{null,null,null,null,r,r,null,null,null,null,null,r,r,null,null},
-//							{null,null,null,null,r,r,null,null,null,null,null,r,r,null,null},
-//							{null,null,null,null,r,r,null,null,null,null,null,r,r,null,null},
-//							{null,null,null,null,r,d,null,null,null,null,null,r,r,null,null},
-//							{null,null,null,null,null,null,null,null,null,null,null,r,r,null,null},
-//							{null,null,null,null,null,null,null,null,null,d,r,r,r,null,null},
-//							{null,null,null,null,null,null,null,null,null,r,r,d,r,null,null},
-//							{null,null,null,null,null,null,null,null,null,null,null,null,null,null,null},
-//							{null,null,null,null,null,null,null,null,null,null,null,null,null,null,null},
-//						};
-//		
-//		l = new OutsideLocation("Test", "This is a test Location", tiles, rooms);
-//		
-//		
-		
 
 		// Rotates array depending on direction and then rendering
 		switch(direction){
@@ -173,93 +130,87 @@ public class RenderingWindow extends JPanel{
 			isometric(tiles,rooms, offgc);
 			break;
 		case EAST:
-			isometric(rotate(tiles), rotate(rooms), offgc);		
-			break;			
-		case SOUTH:
-			isometric(rotate(rotate(tiles)), rotate(rotate(rooms)), offgc);		
-			break;	
-		case WEST:
-			isometric(rotate(rotate(rotate(tiles))), rotate(rotate(rotate(rooms))), offgc);	
+			isometric(rotate(tiles), rotate(rooms), offgc);
 			break;
-				
+		case SOUTH:
+			isometric(rotate(rotate(tiles)), rotate(rotate(rooms)), offgc);
+			break;
+		case WEST:
+			isometric(rotate(rotate(rotate(tiles))), rotate(rotate(rotate(rooms))), offgc);
+			break;
+
 		}
 
-			
+
 			g.drawImage(offscreen,0,0,null);
 
 		}
-		
-	
-		
+
+
+
 		/**
 		 * Iterates through arrays of tiles drawing the map terrain, then iterates through
 		 * room tiles to draw buildings depending on the arrangement of tiles.
-		 * 
+		 *
 		 * new x position = x/2 + y/2 ( + or - constants to fit properly such as imageHeight)
 		 * new y position = y/4 - x/4 ( + or - constants to fit properly such as imageHeight)
-		 * 
-		 * 
+		 *
+		 *
 		 * @param tiles - 2D array of terrain to be drawn
 		 * @param rooms - 2D array of rooms to be drawn
 		 * @param offgc - the graphics
 		 */
-		public void isometric(Tile[][] tiles, Tile[][] rooms, Graphics offgc){			
+		public void isometric(Tile[][] tiles, Tile[][] rooms, Graphics offgc){
+			updateCamera();
+
 			offgc.fillRect(0,0,this.getWidth(), this.getHeight());
-			
+
 			// outside tiles
 			for(int i = 0; i < tiles.length; i++){
 				for(int j = tiles[i].length-1; j >=0 ; j--){
 					FloorTile t = (FloorTile) tiles[i][j];
 					if(t!=null) {
 						// DRAWING TERRAIN
-						
+
 						Image image = getImage(t.toString());
-						offgc.drawImage(image, (j*TILESIZE/2) + (i*TILESIZE/2), ((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2 , null);
-						
-						// DRAWING PLAYER 
+						offgc.drawImage(image, (j*TILESIZE/2) + (i*TILESIZE/2) - cameraX, ((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2 - cameraY, null);
+
+						// DRAWING PLAYER
 						if(t.getPos().equals(player.getPosition())){
 							System.out.println(player.getPosition());
-							Image playerImage = null;
-							try {
-								playerImage = ImageIO.read(new File("src/ui/images/player/0.png"));
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-							
-							//offgc.drawImage(playerImage, (j*TILESIZE/2) + (i*TILESIZE/2) + playerImage.getWidth(null)/2, 5, null);
-							offgc.drawImage(playerImage, (j*TILESIZE/2) + (i*TILESIZE/2) + playerImage.getWidth(null)/2, ((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2  - playerImage.getHeight(null)/2, null);
+							offgc.drawImage(playerImage, (j*TILESIZE/2) + (i*TILESIZE/2) + playerImage.getWidth(null)/2 - cameraX, ((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2  - playerImage.getHeight(null)/2 - cameraY, null);
 						}
-						
+
 						// DRAWING ROOMS
 						if(rooms!=null){
 							Tile r = rooms[i][j];
-							if(r!=null) {					
+							if(r!=null) {
 								// Drawing 2 block high walls
 								if(r instanceof EntranceExitTile){
 									if(j-1 >= 0 && rooms[i][j-1]==null){
-										offgc.drawImage(doorUD, (j*TILESIZE/2) + (i*TILESIZE/2), ((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2 -TILESIZE/2, null);
+										offgc.drawImage(doorUD, (j*TILESIZE/2) + (i*TILESIZE/2) - cameraX, ((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2 -TILESIZE/2 - cameraY, null);
 									} else {
-										offgc.drawImage(doorLR, (j*TILESIZE/2) + (i*TILESIZE/2), ((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2 -TILESIZE/2, null);
+										offgc.drawImage(doorLR, (j*TILESIZE/2) + (i*TILESIZE/2) - cameraX, ((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2 -TILESIZE/2 - cameraY, null);
 									}
 								}
 								else{
-									offgc.drawImage(building, (j*TILESIZE/2) + (i*TILESIZE/2), ((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2 -TILESIZE/2, null);
+									offgc.drawImage(building, (j*TILESIZE/2) + (i*TILESIZE/2) - cameraX, ((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2 -TILESIZE/2 - cameraY, null);
 								}
-								offgc.drawImage(building, (j*TILESIZE/2) + (i*TILESIZE/2), ((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2 -TILESIZE, null);
-								
-	
+								offgc.drawImage(building, (j*TILESIZE/2) + (i*TILESIZE/2) - cameraX, ((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2 -TILESIZE - cameraY, null);
+
+
 								image = building;
-								
+
 								// Western most point of building
 								if(j-1 >= 0 && rooms[i][j-1] == null){
 									image = roofUD;
 								}
-	
+
 								// Northern most point of building
 								if(i+1 < rooms.length && rooms[i+1][j]==null){
 									image = roofLR;
 								}
-	
+
 								// Outwards corner roof
 								if(j-1 >= 0 && i+1<rooms.length && rooms[i][j-1]==null && rooms[i+1][j]==null){
 									image = roofCornerO;
@@ -268,10 +219,10 @@ public class RenderingWindow extends JPanel{
 								if(j-1 >= 0 && i+1 != rooms.length && rooms[i+1][j-1]==null && rooms[i][j-1] != null && rooms[i+1][j]!=null){
 									image = roofCornerI;
 								}
-								
-							
-								offgc.drawImage(image, (j*TILESIZE/2) + (i*TILESIZE/2), (int) (((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2 - (TILESIZE*1.5)), null);
-	
+
+
+								offgc.drawImage(image, (j*TILESIZE/2) + (i*TILESIZE/2) - cameraX, (int) (((i*TILESIZE/4)-(j*TILESIZE/4)) + this.getHeight()/2 - (TILESIZE*1.5)) - cameraY, null);
+
 							}
 						}
 					}
@@ -281,7 +232,7 @@ public class RenderingWindow extends JPanel{
 		}
 
 		/**
-		 * 
+		 *
 		 * Rotates given array of tiles 90 degrees counter-clockwise.
 		 * @param tiles - array to be rotated
 		 * @return rotated array
@@ -295,8 +246,8 @@ public class RenderingWindow extends JPanel{
 			}
 			return newTiles;
 		}
-		
-		
+
+
 		/**
 		 * Sets direction of renderer. May possibly end up being stored in another class?
 		 * @param d - new direction
@@ -304,12 +255,17 @@ public class RenderingWindow extends JPanel{
 		public void setDirection(Game.Direction d){
 			direction = d;
 		}
-		
+
 		/**
 		 * @return current direction of renderer/camera
 		 */
 		public Direction getDirection(){
 			return direction;
+		}
+
+		public void updateCamera(){
+			cameraX = (int) ((player.getPosition().getX()*TILESIZE/2) + (player.getPosition().getY()*TILESIZE/2) + playerImage.getWidth(null)/2 - this.getWidth()/2);
+			cameraY = (int) ((player.getPosition().getY()*TILESIZE/4)-(player.getPosition().getX()*TILESIZE/4)) + this.getHeight()/2  - playerImage.getHeight(null)/2 - this.getHeight()/2;
 		}
 }
 
