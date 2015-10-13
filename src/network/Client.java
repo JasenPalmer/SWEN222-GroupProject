7d644ce16af4881f37b01b86bc2c4c5bd20d372f
Matt Byers <matt_byers@live.com>
2015-10-14 01:25:10 +1300

    Increased buffer again

--- src/network/Client.java
+++ src/network/Client.java
@@ -62,8 +62,8 @@ public class Client {
 		try {
 			System.out.println("Creating socket");
 			socket = new Socket(host, PORT);
-			socket.setReceiveBufferSize(1000);
-			socket.setSendBufferSize(1000);
+			socket.setReceiveBufferSize(128000);
+			socket.setSendBufferSize(128000);
 			socket.setTcpNoDelay(true);
 		} catch (IOException e){
 			System.err.println("Error creating new client: " + user);
--- src/network/Server.java
+++ src/network/Server.java
@@ -68,7 +68,7 @@ public class Server {
 	public void start(){
 		try {
 			serverSocket= new ServerSocket(PORT);
-			serverSocket.setReceiveBufferSize(1000);
+			serverSocket.setReceiveBufferSize(128000);
 			console.displayEvent("Server started successfully on port number: " + PORT);
 
 			while(!finished) {
